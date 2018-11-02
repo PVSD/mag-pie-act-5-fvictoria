0c1f14a8a8d79c058bb72d1875b6717eaa0b9d09
fvictoria <felipevictoria630@yahoo.com>
2018-11-01 20:38:35 -0400

    commit

--- src/Magpie5.java
+++ src/Magpie5.java
@@ -284,7 +284,11 @@ public class Magpie5
 	private String [] randomResponses = {"Interesting, tell me more",
 			"Hmmm.",
 			"Do you really think so?",
-			"You don't say."
+			"You don't say.",
+			"???",
+			"Why though?",
+			"Is it worth it?",
+			"What is that?"
 	};
 	
 }
